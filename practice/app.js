bcccadebe8cf5814f402b3c65a909f0a559a8721
yoitete <kh.70239568@gmail.com>
2026-07-16 15:28:29 +0900

    add: 初回コミット

---
+++ practice/app.js
@@ -0,0 +1,26 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+let longText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+console.log(longText);
+console.log(longText);
+console.log(longText);
+
+let name = 'ジョニー';
+let greet = '私は' + name + '!';
+console.log(greet);
+
+let numTen = 10;
+let strTen = '10';
+console.log(numTen, strTen); 
+
+let minus = -10;
+let decimal = 1.1;
+console.log(minus, decimal);
+
+
+
